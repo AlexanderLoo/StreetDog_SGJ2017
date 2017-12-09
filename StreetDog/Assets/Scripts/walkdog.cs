15eadfaa023139c9bd6daf402fb0b62dfb02f4ae
Andres <afrias4b@hotmail.com>
2017-12-09 02:09:02 -0500

    agua lentea

--- StreetDog/Assets/Scripts/walkdog.cs
+++ StreetDog/Assets/Scripts/walkdog.cs
@@ -8,6 +8,7 @@ public class walkdog : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
 	float movex;
+	bool onWater=false;
 
 	void Start()
 	{
@@ -17,15 +18,46 @@ public class walkdog : MonoBehaviour {
 	void FixedUpdate()
 	{
 		movex = 0;
-		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) 
+		if (onWater) 
 		{
-			movex = -speed;
-		}
-		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+			if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+			{
+				movex = -speed + 1.75f;
+			}
+			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
+			{
+				movex = speed - 1.75f;
+			}
+		} 
+		else 
 		{
-			movex = speed;
+			if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+			{
+				movex = -speed;
+			}
+			if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
+			{
+				movex = speed;
+			}
 		}
-
+		print (onWater);
 		rb2d.velocity = new Vector2 (movex,rb2d.velocity.y);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		
+		if (other.gameObject.tag == "agua") 
+		{
+			onWater = true;
+
+		}
+	
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "agua")
+		{
+			onWater = false;
+		}
 	}
 }
