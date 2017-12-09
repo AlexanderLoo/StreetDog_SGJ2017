28022bbb7cbbb8acfff4118a1471254156d0e68b
AlexanderLoo <AlexanderLoo>
2017-12-09 18:15:34 -0500

    lógica de corazones

--- StreetDog/Assets/Scripts/GameController.cs
+++ StreetDog/Assets/Scripts/GameController.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public static GameController instance;
 	//Los siguientes booleanos verifican si se encontró al enemigo determinado	
 	public bool chinoLoco, peleaPareja;
+	public Image[] motherHeartSprite;
+	public Image[] childHearthSprite;
+
+	public float checkTime = 20;
+	private float checkPoint = 100;
+	//Indices actuales de las listas 
+	private int motherIndex = 0;
+	private int childrenIndex = 0;
 
 	void Awake(){
 
@@ -18,4 +27,32 @@ public class GameController : MonoBehaviour {
 			Destroy (gameObject);
 		}
 	}
+
+	void Update(){
+
+
+	}
+	//Función que actualiza el corazón de la madre
+	void MotherHeart(){
+
+		if (transform.position.x > checkPoint) {
+			motherHeartSprite [motherIndex].enabled = false;
+			motherIndex += 1;
+			motherHeartSprite [motherIndex].enabled = true;
+			checkPoint += 100;
+		}
+	}
+	//Función que actualiza el corazón del hijo
+	void ChildHeart(){
+
+		if (Time.time > checkTime) {
+			childHearthSprite [childrenIndex].enabled = false;
+			childrenIndex += 1;
+			childHearthSprite [childrenIndex].enabled = true;
+			checkTime = 0;
+		}
+	}
+
+
+
 }
