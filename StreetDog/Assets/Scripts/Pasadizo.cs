309a25136a450292a77fb77bf7944c5cbabc805b
Sebastian Rodriguez (Escombruara) <seb_110@hotmail.com>
2017-12-09 11:23:35 -0500

    Pasadizo

---
+++ StreetDog/Assets/Scripts/Pasadizo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pasadizo : MonoBehaviour {
+	public GameObject capaActual;
+	public GameObject capaSiguiente;
+	void OnTriggerStay2D(Collider2D obj)
+	{
+		if (obj.gameObject.tag == "Player")
+		{
+			if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+				CambiarCapas();
+		}
+	}
+	public void CambiarCapas()
+	{
+		capaActual.SetActive(false);
+		capaSiguiente.SetActive(true);
+		
+	}
+}
