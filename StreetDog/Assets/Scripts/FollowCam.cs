d26124f4fcd1cd2e33fdd2ae82556f7a2f661c8c
Andres <afrias4b@hotmail.com>
2017-12-08 23:22:10 -0500

    scripts camara y movimiento perro

---
+++ StreetDog/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour 
+{
+	public Transform Player;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+			TrackPlayer ();
+	}
+	void TrackPlayer()
+	{
+		float targetX = Player.position.x;
+		Vector3 v = new Vector3 (targetX,transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, v, Time.deltaTime * 3f);
+
+	}
+}
---
+++ StreetDog/Assets/Scripts/walkdog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkdog : MonoBehaviour {
+
+	public float speed;             
+
+	private Rigidbody2D rb2d;
+	float movex;
+	// Use this for initialization
+	void Start()
+	{
+		
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void FixedUpdate()
+	{
+		movex = 0;
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			movex = -3f;
+			transform.localScale = new Vector3 (-4f, 4f, 1f);
+
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			movex = 3f;
+			transform.localScale = new Vector3 (4f, 4f, 1f);
+		}
+		rb2d.velocity = new Vector2 (movex,rb2d.velocity.y);
+	}
+}
