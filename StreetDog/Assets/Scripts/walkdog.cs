61cf1a31651bec4c505328ccce090f1b4062fd2b
Sebastian Rodriguez (Escombruara) <seb_110@hotmail.com>
2017-12-09 09:01:31 -0500

    Ladrido de Perro

--- StreetDog/Assets/Scripts/walkdog.cs
+++ StreetDog/Assets/Scripts/walkdog.cs
@@ -8,6 +8,9 @@ public class Walkdog : MonoBehaviour {
 	private Rigidbody2D rb2d;
 	public static Walkdog instance;
 
+	public GameObject ladrido;//Esto solo será un sonido. no creara un GameObject
+	public Transform posBoca;
+
 	public float speed;             
 	public Vector2 gameLimits;
 	float movex;
@@ -26,6 +29,9 @@ public class Walkdog : MonoBehaviour {
 		Vector3 newPos = transform.position;
 		newPos.x = Mathf.Clamp (transform.position.x, gameLimits.x, gameLimits.y);
 		transform.position = newPos;
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			Ladrar();
 	}
 
 	void FixedUpdate()
@@ -36,7 +42,7 @@ public class Walkdog : MonoBehaviour {
 		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
 		{
 			sr.flipX = true;
-
+			
 			//Si tocamos el charco reducimos la velocidad
 			if (onWater) 
 			{
@@ -45,6 +51,9 @@ public class Walkdog : MonoBehaviour {
 			}
 			else
 				movex = -speed;
+
+
+			posBoca.localPosition = new Vector3 (-2.75f,0.9f,0f);
 		}
 		//Misma lógica que la anterior pero hacia el otro lado
 		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
@@ -58,8 +67,15 @@ public class Walkdog : MonoBehaviour {
 			{
 				movex = speed;
 			}
+
+
+			posBoca.localPosition = new Vector3 (2.75f,0.9f,0f);
+			
 		}
 		rb2d.velocity = new Vector2 (movex,rb2d.velocity.y);
+
+		if (rb2d.velocity == Vector2.zero)
+			posBoca.localPosition = new Vector3 (1.25f,0.9f,0f);
 	}
 
 	public void SetWater(bool agua)
@@ -70,5 +86,10 @@ public class Walkdog : MonoBehaviour {
 	public void ActualizarSpeed (float _valor)
 	{
 		speed *= _valor;
+	}
+
+	public void Ladrar ()
+	{
+		Destroy (Instantiate(ladrido,posBoca.position,transform.rotation),1f);
 	}
 }
