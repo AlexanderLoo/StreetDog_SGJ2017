f8f954a72237db7bb1ee296b73fcfb1d778a132d
Andres <afrias4b@hotmail.com>
2017-12-09 01:16:56 -0500

    chino cuchillo

---
+++ StreetDog/Assets/Scripts/ThrowKnife.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowKnife : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject cuchillo;
+	float elapsed=0;
+	public float intervaloDisparo;
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		elapsed += Time.deltaTime;
+		if (elapsed >= intervaloDisparo)
+		{
+			Instantiate (cuchillo, transform.position, transform.rotation);
+			elapsed = 0;
+		}
+	}
+}
---
+++ StreetDog/Assets/Scripts/kniveMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kniveMove : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody2D body;
+	public float speedx;
+	public float speedy;
+	float elapsed;
+	void Awake () 
+	{
+		body = GetComponent<Rigidbody2D> ();
+	}
+	void Start()
+	{
+		body.AddTorque (speedx*100);
+		body.velocity = new Vector2 (-speedx, speedy*3);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		elapsed += Time.deltaTime;
+		if (elapsed >= 2) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
