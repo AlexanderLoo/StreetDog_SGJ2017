66f265c52b3bf82d120afb022e1aa347279f5359
Sebastian Rodriguez (Escombruara) <seb_110@hotmail.com>
2017-12-09 16:06:44 -0500

    Avance del nivel

---
+++ StreetDog/Assets/Scripts/Paralax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour {
+
+	public int capa;
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer>().sortingOrder = capa * -100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate (Vector3.right* (10-capa)*Time.deltaTime);
+
+		}
+			
+	}
+}
