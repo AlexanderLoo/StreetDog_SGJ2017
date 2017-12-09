a0d6d9edc48ca5b961cd1534250cba00e3c6202c
AlexanderLoo <AlexanderLoo>
2017-12-09 08:03:14 -0500

    ....

--- StreetDog/Assets/Scripts/KnivesThrow.cs
+++ StreetDog/Assets/Scripts/KnivesThrow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectThrow : MonoBehaviour {
+public class KnivesThrow : MonoBehaviour {
 
 	private Rigidbody2D body;
 	//longitud al azar
