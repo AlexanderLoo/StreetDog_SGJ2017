873ee818b8a21381650f9d43d3d513a4b7ede6d4
AlexanderLoo <AlexanderLoo>
2017-12-09 03:24:55 -0500

    Flipear Sprite del perro

--- StreetDog/Assets/Scripts/walkdog.cs
+++ StreetDog/Assets/Scripts/walkdog.cs
@@ -7,36 +7,42 @@ public class walkdog : MonoBehaviour {
 
 	public float speed;             
 
+	private SpriteRenderer sr;
 	private Rigidbody2D rb2d;
+	private 
 	float movex;
 	bool onWater=false;
 	public static walkdog instance;
 
 	void Awake()
 	{
+		sr = GetComponent<SpriteRenderer> ();
 		rb2d = GetComponent<Rigidbody2D> ();
 		instance = this;
-	}
-	void Start()
-	{
-
 	}
 
 	void FixedUpdate()
 	{
 		movex = 0;
 
+		//Si nos movemos a la izquierda flipeamos el sprite para que mire hacia la izquierda
 		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
 		{
+			sr.flipX = true;
+
+			//Si tocamos el charco reducimos la velocidad
 			if (onWater) 
 			{
 				movex = -speed + 1.75f;
+				//movex = -speed * 0.75f;(otra opción)
 			}
 			else
 				movex = -speed;
 		}
+		//Misma lógica que la anterior pero hacia el otro lado
 		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
 		{
+			sr.flipX = false;
 			if (onWater) 
 			{
 				movex = speed - 1.75f;
--- StreetDog/Assets/watercoll.cs
+++ StreetDog/Assets/watercoll.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class watercoll : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 	void OnTriggerStay2D(Collider2D other)
 	{
 
