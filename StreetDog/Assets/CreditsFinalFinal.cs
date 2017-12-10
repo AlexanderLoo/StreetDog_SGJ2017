eceee8bd665e39d65a75a0c02d04c9d3d7ad758f
Andres <afrias4b@hotmail.com>
2017-12-10 13:18:40 -0500

    final final

---
+++ StreetDog/Assets/CreditsFinalFinal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Prime31.TransitionKit;
+
+public class CreditsFinalFinal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			var wind = new FadeTransition () {
+				nextScene = 2,
+				duration = 3.4f,
+
+			};
+			TransitionKit.instance.transitionWithDelegate (wind);
+		}
+	}
+}
